0141f12bb6346f9784ec693fa136d245ed013fe7
Anna Alimova <new@alimov.net>
2016-12-24 21:06:15 +0200

    Object parsed, get works

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
  * Created by alimov on 12/17/16.
  */
 const xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://jservice.io/api/random', false);
-xhr.send();
-var resp = JSON.parse(xhr.response);
+function Reload(){
+    xhr.open('GET', 'http://jservice.io/api/random', false);
+    xhr.send();
+}
+Reload();
 
-document.getElementById("num").innerHTML = resp.id;
+//var resp = JSON.parse(xhr.response);
+
+//document.getElementById("num").innerHTML = resp.id;
 //    + "<br>" +
 //    resp.question + "<br>" +
 //    resp.answer;
 
 console.log(xhr.response);
-console.log(document);
