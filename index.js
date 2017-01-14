b12f918e3c0053061699b467ac5452b71e8d889a
Anna Alimova <new@alimov.net>
2017-01-14 11:10:51 +0200

    small tweaks

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
 /**
  * Created by alimov on 12/17/16.
  */
+var answer;
+var correct = 0;
+var total = 0;
 var xhr = new XMLHttpRequest();
 function Reload(){
-    xhr.open('GET', 'http://jservice.io/api/random', false);
+    xhr.open('GET', 'https://jservice.io/api/random', false);
     xhr.send();
-//    var resp = JSON.parse(xhr.response);
 }
 Reload();
+var resp = JSON.parse(xhr.response);
 
-//var resp = JSON.parse(xhr.response);
-
-//document.getElementById("num").innerHTML = resp.id;
-//    + "<br>" +
-//    resp.question + "<br>" +
-//    resp.answer;
+function findRow(node){
+    var i=1;
+    while( (node = node.previousSibling) != null ) {
+        i++;
+    }
+    return i;
+}
+function updateAnswer(node){
+    //cut off letter from var answer
+    var l = findRow(node);
+    var length = answer.length;
+    answer = answer.slice(0, l-1)+answer.slice(l, length-1);
+}
 
 console.log(xhr.response);
+
+/* rewrite to async
+
+
+ var xhr = new XMLHttpRequest();
+
+ xhr.open('GET', 'phones.json', true);
+
+ xhr.send(); // (1)
+
+ xhr.onreadystatechange = function() { // (3)
+ if (xhr.readyState != 4) return;
+
+ button.innerHTML = 'Готово!';
+
+ if (xhr.status != 200) {
+ alert(xhr.status + ': ' + xhr.statusText);
+ } else {
+ alert(xhr.responseText);
+ }
+
+ }
+
+ button.innerHTML = 'Загружаю...'; // (2)
+ button.disabled = true;
+ */
